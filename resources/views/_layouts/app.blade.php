20e421295940366caf6f06daf0d4c03952ba1860
bimoalfarrabi <46051173+bimoalfarrabi@users.noreply.github.com>
2022-11-16 21:17:36 +0700

    Completing course and added personal theme.

--- resources/views/_layouts/app.blade.php
+++ resources/views/_layouts/app.blade.php
@@ -106,11 +106,10 @@
           <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
             <!-- Content -->
             <h6 class="text-uppercase fw-bold mb-4">
-              <i class="fas fa-gem me-3 text-secondary"></i>Company name
+              <i class="fas fa-gem me-3 text-secondary"></i>Tentang Kami
             </h6>
             <p>
-              Here you can use rows and columns to organize your footer content. Lorem ipsum
-              dolor sit amet, consectetur adipisicing elit.
+              Sebuah komunitas heker paling disegani di seluruh dunia dan alam semesta raya.
             </p>
           </div>
           <!-- Grid column -->
@@ -140,7 +139,7 @@
           <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
             <!-- Links -->
             <h6 class="text-uppercase fw-bold mb-4">
-              Useful links
+              Not so Useful links
             </h6>
             <p>
               <a href="#!" class="text-reset">Pricing</a>
@@ -160,14 +159,13 @@
           <!-- Grid column -->
           <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
             <!-- Links -->
-            <h6 class="text-uppercase fw-bold mb-4">Contact</h6>
-            <p><i class="fas fa-home me-3 text-secondary"></i> New York, NY 10012, US</p>
+            <h6 class="text-uppercase fw-bold mb-4">Kontak</h6>
+            <p><i class="fas fa-home me-3 text-secondary"></i> [REDACTED ADRESS]</p>
             <p>
               <i class="fas fa-envelope me-3 text-secondary"></i>
-              info@example.com
+              [REDACTED EMAIL]
             </p>
-            <p><i class="fas fa-phone me-3 text-secondary"></i> + 01 234 567 88</p>
-            <p><i class="fas fa-print me-3 text-secondary"></i> + 01 234 567 89</p>
+            <p><i class="fas fa-phone me-3 text-secondary"></i>[REDACTED PHONE NUMBER]</p>
           </div>
           <!-- Grid column -->
         </div>
